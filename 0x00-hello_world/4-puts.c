41ee05d89106502c0dad443c57131fee8c98515d
Isaac Oyero <ayo22207@gmail.com>
2023-06-15 07:31:48 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - demonstrating use of puts
- *
- *Return:returns zero for void 
- * 
- */
-
+* main - demonstrating use of puts
+*
+*Return:returns zero for void 
+* 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
