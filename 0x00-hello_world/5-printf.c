82aa6f2ad3d193eccd27f341cbce53a00b11e33b
Isaac Oyero <ayo22207@gmail.com>
2023-06-15 07:46:18 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - using printf
+ *
+ * return : returns 0 upon success
+ */
+int main(void)
+{ 
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
