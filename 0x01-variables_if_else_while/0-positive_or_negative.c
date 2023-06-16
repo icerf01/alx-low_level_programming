65b46a4032f5aaeb526891dd23756e987d8de39c
Isaac Oyero <ayo22207@gmail.com>
2023-06-16 06:47:26 -0700

    return if no is pos or neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
-	{
-		printf("%Lf is positive\n",n);
-	}
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-	{
-		printf("%Lf is negative\n",n);
-	}
-	
+		printf("%d is negative\n", n);
+
 	return (0);
 }
