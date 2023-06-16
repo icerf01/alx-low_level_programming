e273a68b79da5f5bf2f3c4316407b3a0b1fbbe1c
Isaac Oyero <ayo22207@gmail.com>
2023-06-16 11:42:53 -0700

    a-z in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *main - print a-z in reverse order
+ * *
+ * *Return: return 0 upon success
+ * */
+int main(void)
+{
+	char l;
+			
+	for (l = 'z'; l >= 'a'; l--)
+		putchar(l);
+	putchar('\n');
+	
+	return (0);
+}
