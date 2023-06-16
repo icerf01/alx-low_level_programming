d2adbbf65d629eebd4309cdee4eeebd811765ec9
Isaac Oyero <ayo22207@gmail.com>
2023-06-16 09:12:51 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print a-z A-Z
+ *
+ * Return: return 0 upon success
+ */
+
+int main(void)
+{
+	char l,u;
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (u = 'A'; u <= 'Z'; u++)
+		putchar(u);
+	putchar("\n");
+	return(0);
+}
