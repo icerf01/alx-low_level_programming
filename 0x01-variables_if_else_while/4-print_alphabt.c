52054ef096c489721a8b78970972115eaccfc893
Isaac Oyero <ayo22207@gmail.com>
2023-06-16 09:48:19 -0700

    a-z excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - a-z excluding q and e
+ *
+ *Return: return 0 upon success
+ */
+int main(void)
+{
+	char 
+	for (l = 'a'; l <= 'z'; l++)
+		if (l == 'e' || l == 'q')
+			break;
+		putchar(l);
+	putchar ('\n');
+	return (0);
+}
