9cf5fd9d0f23b2178af3363778f6b4044f4ce363
Isaac Oyero <ayo22207@gmail.com>
2023-06-16 10:01:11 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,14 @@ int main(void)
 {
 	char l; 
 	for (l = 'a'; l <= 'z'; l++)
-		if (l == 'e' && l == 'q')
+	{
+		if (l == 'e' ||	l == 'q')
+		{
 			break;
+		}
 		putchar(l);
+	}
 	putchar ('\n');
+
 	return (0);
 }
