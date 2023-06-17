78ea16468cb1a611f7463e0cf76eed3e2820fd06
Isaac Oyero <ayo22207@gmail.com>
2023-06-17 04:38:38 -0700

    print hexa digits in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print hexa numbers
+ *
+ * Return: return 0 upon success
+ */
+int main(void)
+{
+	char h;
+
+	for (h = '0'; h < 10; h++)
+		putchar(h);
+	for (h = 'a'; h <= 'f'; h++)
+		putchar(h);
+	putchar('\n');
+
+	return (0);
+}
