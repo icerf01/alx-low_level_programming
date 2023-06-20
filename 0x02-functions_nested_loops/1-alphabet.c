bc2e75896c217e4ecc2f55d7fbed71314e1fa6e2
Isaac Oyero <ayo22207@gmail.com>
2023-06-20 09:15:52 -0700

    alphabet a-z using header file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints a-z using the main.h
+ *
+ * Return: returns 0 upon success
+ */
+int main(void)
+{
+	la();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,13 @@ int pp(void)
 	printf("_putchar\n");
 	return (0);
 }
+
+int la(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
