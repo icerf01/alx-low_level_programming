255ad579956416f06374ec9cd7837c04ae3dc68c
Isaac Oyero <ayo22207@gmail.com>
2023-06-21 02:15:34 -0700

    print a-z 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,11 +17,12 @@ void print_alphabet(void)
 
 void print_alphabet_x10(void)
 {
-	char l10, char a;
+	char l10;
+	char a;
 
 	for (a = 0; a < 10; a++ )
 	{
-		for (l = 'a'; l <= 'z'; l++ )
+		for (l10 = 'a'; l10 <= 'z'; l10++ )
 			putchar(l);
 		putchar('\n');
 	}
