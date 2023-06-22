51ca06611a942cb24870a4ea4d29b108059c52fc
Isaac Oyero <ayo22207@gmail.com>
2023-06-22 13:53:29 -0700

    check if argument is digit (0-9)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * _isdigit - this function checks if input
+ * is a digit (0-9)
+ *
+ * Return: return 0 if postive, else ret 1
+ */
+int _isdigit(int c)
+{
+	int a;
+	int b = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (c == a)
+			b = 1;
+	}
+	return (b);
+}
