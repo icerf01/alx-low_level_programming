da4e8d9fd47b25ee28aa4f0d89f740b715232200
Isaac Oyero <ayo22207@gmail.com>
2023-06-22 04:24:34 -0700

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,7 +11,7 @@ int _islower(int c)
 	char a[27];
 	int i = 0;
 	int b;
-	int d = 0;
+	int d = 48;
 
 	for(alp = 'a'; alp <= 'z'; alp++)
 	{
@@ -21,7 +21,7 @@ int _islower(int c)
 	for(b = 0; b <= 26; b++)
 	{
 		if (c == a[b])
-			d = 1;
+			d = 49;
 	}
 
 	return (d);
