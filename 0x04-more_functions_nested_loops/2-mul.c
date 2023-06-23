2ceba381f30463202f69df30502d7565c196f6d5
Isaac Oyero <ayo22207@gmail.com>
2023-06-23 02:23:05 -0700

    multiply 2 ints

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,9 @@
  * mul - function to multiply 2 integers
  * @a: user input
  * @b: user input
- * @r: stores result
  * Return: return result r
  */
 int mul(int a, int b)
 {
-	int r;
-
-	r = a * b;
-
-	return (r);
+	return (a * b);
 }
