afbed3d1fe01fd9b5eb3fe99e7ce5e896cf206e0
Isaac Oyero <ayo22207@gmail.com>
2023-06-23 02:30:08 -0700

    0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 \n
+ * @a: enumerates 0-9
+ * Return: void
+ */
+void print_numbers()
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar("\n");
+}
