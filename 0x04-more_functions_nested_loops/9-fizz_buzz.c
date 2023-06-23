af5c59131d87a1e0d5d51408713d2e5f76622452
Isaac Oyero <ayo22207@gmail.com>
2023-06-23 04:17:27 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,9 @@ void fizz_buzz(void)
 
 	for(; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i%3 == 0 && i % 5 == 0)
+			printf("fizzbuzz ");
+		else if (i % 3 == 0)
 			printf("fizz ");
 		else if (i % 5 == 0)
 			printf("buzz ");
