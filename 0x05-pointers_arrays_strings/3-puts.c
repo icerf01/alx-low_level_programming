51a311616b3072c200e84c6bbf067526dd98a83e
Isaac Oyero <ayo22207@gmail.com>
2023-06-26 07:43:55 -0700

    _puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * _puts - function that print a string
+ * followed by a new line
+ * @str: string input var
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
