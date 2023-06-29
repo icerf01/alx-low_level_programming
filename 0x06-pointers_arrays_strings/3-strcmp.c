c8604909a71862b54e4a96140b0ecff8a68b0484
Isaac Oyero <ayo22207@gmail.com>
2023-06-29 12:26:53 -0700

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * —strcmp - function that compares
+ * two strings
+ * @s1: first var
+ * @s@: second var 
+ * Return: evaluate a-b
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return *s1 - *s2;
+}
