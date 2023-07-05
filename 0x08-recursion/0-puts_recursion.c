90a22566abc6f95d7e49f814d2da029f0b0013fc
Isaac Oyero <ayo22207@gmail.com>
2023-07-05 11:35:38 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,9 @@
  * _puts_recursion - function that
  * returns a string with a new line
  * @s: pointer to string input
- * @Return: void
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	printf("%s\n",s);
+	printf("%s\n", s);
 }
