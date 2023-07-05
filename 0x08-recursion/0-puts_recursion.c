05acb61a5a6b0cb3791f3df22b5c1cdd0e5f1161
Isaac Oyero <ayo22207@gmail.com>
2023-07-05 11:13:54 -0700

    recursive puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that
+ * returns a string with a new line
+ * @s: pointer to string input
+ * @Return: void
+ */
+void _puts_recursion(char *s)
+	return ("%s\n",*s);
