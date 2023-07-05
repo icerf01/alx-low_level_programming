d44d56246f7498b724707de61bad64ce28d23816
Isaac Oyero <ayo22207@gmail.com>
2023-07-05 12:28:49 -0700

    str length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to
+ * get string length
+ * @s: char pointer
+ * Return: return int str length
+ */
+_strlen_recursion(char *s)
+{
+	int i = 0;
+
+	while(s)
+		i++;
+	return (i);
+}
