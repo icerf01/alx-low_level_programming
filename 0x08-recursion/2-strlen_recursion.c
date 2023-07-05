21607cead5cdf1f71208fccf70d4129427c962d8
Isaac Oyero <ayo22207@gmail.com>
2023-07-05 12:41:13 -0700

    str length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,11 +6,7 @@
  * @s: char pointer
  * Return: return int str length
  */
-_strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if(s*)
-		i++;
-	return (i);
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
 }
