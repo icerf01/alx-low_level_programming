f6f39cf2c513b614e7ed0961160c92b278e17802
Isaac Oyero <ayo22207@gmail.com>
2023-07-17 10:34:47 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * struct dog - struct for dog
+ * @name: char for dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog {
+	char name[30];
+	float age;
+	char owner[30];
+}
