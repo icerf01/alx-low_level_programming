e3219fe28e951a28508ea42c36b6327d0f2c12d4
Isaac Oyero <ayo22207@gmail.com>
2023-07-17 13:35:34 -0700

    intializing struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = &dog;
-	(*p).name = name;
-	(*p).age = age;
-	(*p).owner = owner;
+	d = &dog
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
