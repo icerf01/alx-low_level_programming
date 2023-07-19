bc7cd8ff5337e3213e867d4181db62999deeeae3
Isaac Oyero <ayo22207@gmail.com>
2023-07-19 10:32:37 -0700

    Function to print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,11 @@
  * print_name - A function that 
  * prints a name.
  * @name: pointer to name to print
- * @f: function to print name
- * Return: Nothing
+ * @f:pontr 2 function to print name
+ * Return: void
  */
-void print_name(char *name, void (*f)(char *name))
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	f = &printname;
+	return (name);
 }
