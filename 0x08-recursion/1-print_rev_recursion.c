fbb570b05ef22531b1885a37751484df461854b6
Isaac Oyero <ayo22207@gmail.com>
2023-08-30 14:09:23 +0100

    function that prints string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * void _print_rev_recursion - function that prints a string in reverse.
+ *_print_rev_recursion - function that prints a string in reverse.
  * @s: string pointer.
  * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s);
-	int index = len - 1;
-
-	if (*s)
-		putchar('\n');
-	putchar(*(s + index));
-	_print_rev_recursion(s + (index - 1));
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s +1 );
+		putchar(*s);
+	}
 }
-
