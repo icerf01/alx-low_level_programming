23e947cb8d5c211b7691dd240df169a92783a1af
Isaac Oyero <ayo22207@gmail.com>
2023-09-14 02:58:37 +0100

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - A function that prints a name.
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * array_iterator - executes a function given as a
