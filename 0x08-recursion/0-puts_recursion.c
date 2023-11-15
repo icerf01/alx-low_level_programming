56577723c0b13ebef2ef92f6be414dd05ba3a6c8
Isaac Oyero <ayo22207@gmail.com>
2023-11-15 12:19:18 +0100

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion-function to print a string then new line
+ * @s: string input
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s != '\0')
+	{
+		putchar(*s);
+		_puts_recursion(*s)
+	}
+	else
+		putchar('\n');
+}
