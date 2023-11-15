672afc278ed27590721a7e666388254f5bec3d1e
Isaac Oyero <ayo22207@gmail.com>
2023-11-15 13:09:07 +0100

    recursive function to return a number x^y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - recursive function that returns the power of 2 inputs
+ * @x: number
+ * @y: power
+ * return: x^y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
