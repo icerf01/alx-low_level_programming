2691d66c5ebfcfdc7b15d0b6efaa7e7a4b20b4e5
Isaac Oyero <ayo22207@gmail.com>
2023-11-15 12:47:48 +0100

    recursive str len

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion- function to get string length
+ * @s: string input
+ * Return: int str len
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1);
+}
+
